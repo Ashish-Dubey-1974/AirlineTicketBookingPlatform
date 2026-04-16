6ae190913e7f9d77db04456d20a92120bd6e59bb
Ashish Dubey <adg1974sop@gmail.com>
2026-04-16 19:08:10 +0530

    Delete SkyBooker.Payment.API directory

--- SkyBooker.Payment.API/DTOs/ErrorResponseDto.cs
+++
@@ -1,9 +0,0 @@
-namespace SkyBooker.Payment.API.DTOs;
-
-public class ErrorResponseDto
-{
-    public string Message { get; set; } = string.Empty;
-    public string? Details { get; set; }
-    public int StatusCode { get; set; }
-    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
-}
